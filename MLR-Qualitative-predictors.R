640d81974695bda782823978aecadd68a1d8de3b
umangU <72771903+umangU@users.noreply.github.com>
2021-07-26 23:18:33 +1000

    Code Updated

--- MLR-Qualitative-predictors.R
+++ MLR-Qualitative-predictors.R
@@ -4,8 +4,8 @@ library(ISLR)
 attach(Carseats)
 names(Carseats)
 str(Carseats)
-# MLR using all predictors and some interaction terms
+#MLR using all predictors and some interaction terms
 lm.fit = lm(Sales~. +Income:Advertising + Price:Age, data = Carseats)
 summary(lm.fit)
-# Contrast function to find out Qualitative variable encoding
+#Contrast function to find out Qualitative variable encoding
 contrasts(ShelveLoc)
